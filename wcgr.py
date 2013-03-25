0296d62217c0385a6dad5d8e6eb351a80b50842b
Nevik Rehnel <hai.kataker@gmx.de>
2013-03-25 22:24:58 +0100

    Make code more general; options; element spec
    Start making code more general. I understand how to use BeautifulSoup to grab
    the contents of a web page, so now it's time to build a program around it.
    
    First of all, I added some program options using the argparse module provided by
    Python. This is a real blessing and makes working with command line parameters
    very easy.
    
    Then I started the parsing of a specifier string for the interesting HTML
    elements. For now, you can extract a string from the HTML by using the specifier
    format <CSS selector>[/<attribute name>], which will select the element by its
    CSS path/selector; if given, the content of the specified attribute is used; if
    this is not given or empty, the tag's string content is used.

--- wcgr.py
+++ wcgr.py
@@ -16,11 +16,13 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ########
 
+import argparse
 import urllib, urlparse
 from bs4 import BeautifulSoup
 
-url = "https://xkcd.com/1/"
+#url = "https://xkcd.com/1/"
 
+##First testing code: (xkcd)
 #soup = BeautifulSoup(urllib.urlopen(url))
 
 #print "URL: ", url
@@ -33,12 +35,42 @@ url = "https://xkcd.com/1/"
 #print "Next: ", urlparse.urljoin(url, soup.find(id="middleContainer").ul.find(rel="next")['href'])
 #print "Prev: ", urlparse.urljoin(url, soup.find(id="middleContainer").ul.find(rel="prev")['href'])
 
-def getImageFrom(url):
-    soup = BeautifulSoup(urllib.urlopen(url))
-    
-    print url, " | ", soup.title.string, " | ", soup.find(id="comic").img['src']
-    
-    return urlparse.urljoin(url, soup.find(id="middleContainer").ul.find(rel="next")['href'])
+##Second testing code: (xkcd)
+#def getImageFrom(url):
+#    soup = BeautifulSoup(urllib.urlopen(url))
+#    
+#    print url, " | ", soup.title.string, " | ", soup.find(id="comic").img['src']
+#    
+#    return urlparse.urljoin(url, soup.find(id="middleContainer").ul.find(rel="next")['href'])
+#
+#for i in range(10):
+#    url = getImageFrom(url)
 
-for i in range(10):
-    url = getImageFrom(url)
+aparser = argparse.ArgumentParser()
+aparser.add_argument("-t", "--title-element", help="the identifier of the comic's issue title")
+aparser.add_argument("url", help="the URL of the first comic page to grab")
+args = aparser.parse_args()
+
+print "URL: ", args.url
+soup = BeautifulSoup(urllib.urlopen(args.url))
+
+## process -t/--title-element flag
+title=''
+if args.title_element != None: # if we actually got the flag
+#print soup.select("#ctitle")[0].string
+    parts = args.title_element.split('/') # split it along slashes (only the first two parts will be used
+    if len(parts) > 0: # if splitting went right (this should always hold, but yaknow, be safe)
+        title_css_path = parts[0] # the first part is the actual CSS selector/path
+        if title_css_path != None and len(title_css_path) > 0: #if the css path is not empty
+            title_attribute = ''
+            if len(parts) > 1: # if we actually have a second part
+                title_attribute = parts[1] # then that second part is the attribute
+            title_element = soup.select(title_css_path) # get the specified element(s)
+            if len(title_element) > 0: # if we got some
+                title_element = title_element[0] # pick only the first one
+                if title_attribute == None or len(title_attribute) == 0: # empty attribute = use tag content
+                    title = title_element.string
+                else: # an attribute was really specified
+                    title = title_element[title_attribute]
+print "Title: ", title
+
